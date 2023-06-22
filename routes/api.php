1437520a6525a28c7fdeb01e97e4aa86530dd802
Florian <b2p-wakiry@hotmail.fr>
2023-06-22 17:54:58 +0200

    Update API route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\ManufacturerController;
+use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
