309fd515059966de857f40352d191ee66b6817ad
Florian <b2p-wakiry@hotmail.fr>
2023-06-22 17:54:22 +0200

    Update API route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('manufacturer', ManufacturerController::class);
 });
