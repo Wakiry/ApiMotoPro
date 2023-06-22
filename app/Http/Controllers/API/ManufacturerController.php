0770a4809670a3953a91bb87e9374dbccb5e381e
Wakiry <dev.pacaudflorian@gmail.com>
2023-06-22 21:13:36 +0200

    Update Controllers

--- app/Http/Controllers/API/ManufacturerController.php
+++ app/Http/Controllers/API/ManufacturerController.php
@@ -2,10 +2,100 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Manufacturer;
+use Illuminate\Http\Response;
+use Validator;
+use App\Http\Resources\ManufacturerResource;
+use Illuminate\Http\JsonResponse;
 
-class ManufacturerController extends Controller
+class ManufacturerController extends BaseController
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $manufacturers = Manufacturer::all();
+        return $this->sendResponse(ManufacturerResource::collection($manufacturers), 'Manufacturers retrieved successfully.');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $manufacturer = Manufacturer::create($input);
+
+        return $this->sendResponse(new ManufacturerResource($manufacturer), 'Manufacturer created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
+    {
+        $manufacturer = Manufacturer::find($id);
+
+        if (is_null($manufacturer)) {
+            return $this->sendError('Manufacturer not found.');
+        }
+        return $this->sendResponse(new ManufacturerResource($manufacturer), 'Manufacturer retrieved successfully.');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Manufacturer $manufacturer
+     * @return JsonResponse
+     */
+    public function update(Request $request, Manufacturer $manufacturer): JsonResponse
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $manufacturer->name = $input['name'];
+        $manufacturer->save();
+
+        return $this->sendResponse(new ManufacturerResource($manufacturer), 'Manufacturer updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Manufacturer $manufacturer
+     * @return JsonResponse
+     */
+    public function destroy(Manufacturer $manufacturer): JsonResponse
+    {
+        $manufacturer->delete();
+
+        return $this->sendResponse([], 'Manufacturer deleted successfully.');
+    }
 }
--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\JsonResponse;
 
 class RegisterController extends BaseController
@@ -26,15 +26,15 @@ class RegisterController extends BaseController
             'c_password' => 'required|same:password',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $this->sendError('Validation Error.', $validator->errors());
         }
 
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-        $success['name'] =  $user->name;
+        $success['token'] = $user->createToken('MyApp')->plainTextToken;
+        $success['name'] = $user->name;
 
         return $this->sendResponse($success, 'User register successfully.');
     }
@@ -47,15 +47,14 @@ class RegisterController extends BaseController
      */
     public function login(Request $request): JsonResponse
     {
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $user = Auth::user();
-            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
-            $success['name'] =  $user->name;
+            $success['token'] = $user->createToken('MyApp')->plainTextToken;
+            $success['name'] = $user->name;
 
             return $this->sendResponse($success, 'User login successfully.');
-        }
-        else{
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
         }
     }
 }
