bebb81aa3feaa77a539ae18a1c45650204b29402
Florian <b2p-wakiry@hotmail.fr>
2023-06-23 15:03:55 +0200

    Update Manufacturer Model

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -10,6 +10,27 @@ class Manufacturer extends Model
     use HasFactory;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'manufacturer';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_manufacturer';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
