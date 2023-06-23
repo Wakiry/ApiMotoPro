0f829b5f73e76a9195bc2d620643b7c718148df0
Florian <b2p-wakiry@hotmail.fr>
2023-06-23 15:59:29 +0200

    Add engine tables

---
+++ database/migrations/2023_06_23_133816_create_engine_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('engine', function (Blueprint $table) {
+            $table->id('id_engine');
+            $table->unsignedBigInteger('id_engine_type');
+            $table->foreign('id_engine_type')->references('id_engine_type')->on('engine_type');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('engine');
+    }
+};
